f6c7d2905375c6bfdd477bc0bdbf45471ad09ded
LifeAsDev <angelocraft551@gmail.com>
2024-03-14 16:49:33 -0400

    event page

---
+++ travel-event-host-app/src/app/event/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+export default function Page({ params }: { params: { id: string } }) {
+  const [eventData, setEventData] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchUrl = `/api/event/${params.id}`;
+        const res = await fetch(fetchUrl, {
+          method: 'GET',
+          headers: { 'Content-type': 'application/json' },
+        });
+        if (res.ok) {
+          const resData = await res.json();
+          setEventData(resData.eventFound);
+        } else {
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [params.id]);
+  return <main></main>;
+}
