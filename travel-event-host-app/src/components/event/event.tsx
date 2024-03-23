1c9c48bcc867c6ba321898116f899f71e5eb61f8
LifeAsDev <angelocraft551@gmail.com>
2024-03-23 11:46:31 -0400

    event skeleton loading

--- travel-event-host-app/src/components/event/event.tsx
+++ travel-event-host-app/src/components/event/event.tsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 import styles from './styles.module.css';
 import Event from '@/models/event';
 import Image from 'next/image';
-export default function Event({ eventData }: { eventData: Event | undefined }) {
+import Skeleton from '@mui/material/Skeleton';
+
+export default function Event({ eventData }: { eventData: Event | undefined | string }) {
+  console.log(eventData);
   const [attendeesMenuIsOpen, setAttendeesMenuIsOpen] = useState(false);
   const [attendeesSearchInput, setAttendeesSearchInput] = useState('');
   return (
@@ -109,66 +112,152 @@ export default function Event({ eventData }: { eventData: Event | undefined }) {
       )}
       <section className={styles.eventDetailsSection}>
         <div className={styles.defaultMargin}>
-          <Image
-            className={styles.eventHeroImage}
-            width={500}
-            height={500}
-            alt=''
-            src={'/images/event/mock-images/mock-image-02.svg'}
-          />
-          <div className={styles.mainInfo}>
-            <h2>Code Event</h2>
-            <div className={styles.dateAndAddress}>8 fev. 2024 02:00 PM - São Paulo, Brazil</div>
-          </div>
-          <p className={styles.hostedBy}>
-            Hosted By
-            <br />
-            Angelo
-          </p>
-          <header>Event Details</header>
-          <p className={styles.description}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <button className={styles.subscribeBtn}>Subscribe to event</button>
+          {eventData ? (
+            <>
+              {' '}
+              <Image
+                className={styles.eventHeroImage}
+                width={500}
+                height={500}
+                alt=''
+                src={'/images/event/mock-images/mock-image-02.svg'}
+              />
+              <div className={styles.mainInfo}>
+                <h2>Code Event</h2>
+                <div className={styles.dateAndAddress}>
+                  8 fev. 2024 02:00 PM - São Paulo, Brazil
+                </div>
+              </div>
+              <p className={styles.hostedBy}>
+                Hosted By
+                <br />
+                Angelo
+              </p>
+              <header>Event Details</header>
+              <p className={styles.description}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+                with desktop publishing software like Aldus PageMaker including versions of Lorem
+                Ipsum.
+              </p>
+              <button className={styles.subscribeBtn}>Subscribe to event</button>
+            </>
+          ) : (
+            <>
+              <Skeleton variant='rectangular' sx={{ borderRadius: 0, height: '320px' }} />
+              <div className={styles.mainInfo}>
+                <Skeleton
+                  variant='text'
+                  sx={{ width: '8em', lineHeight: '1em', fontSize: '2.5em' }}
+                />
+                <Skeleton
+                  variant='text'
+                  sx={{ width: '8em', lineHeight: '1em', fontSize: '2.5em' }}
+                />
+              </div>
+              <p className={styles.hostedBy}>
+                <Skeleton variant='text' sx={{ fontSize: '1.1em', width: '6em' }} />
+                <Skeleton variant='text' sx={{ fontSize: '1.1em', width: '5em' }} />
+              </p>
+              <header>Event Details</header>
+              <p className={styles.description}>
+                <Skeleton variant='text' sx={{}} />
+                <Skeleton variant='text' sx={{}} />
+                <Skeleton variant='text' sx={{}} />
+                <Skeleton variant='text' sx={{}} />
+                <Skeleton variant='text' sx={{}} />
+              </p>
+            </>
+          )}
         </div>
       </section>
       <section className={styles.participantsSection}>
         <div className={styles.defaultMargin}>
-          <header>Speaker</header>
-          <p className={styles.hostName}>Angelo Sarmiento</p>
-          <div className={styles.attendees}>
-            <div className={styles.attendeesTopBox}>
-              <p className={styles.attendeesCount}>Attendees(31)</p>
-              <p onClick={() => setAttendeesMenuIsOpen(true)} className={styles.attendeesSeeAll}>
-                See all
-              </p>
-            </div>
-            <div className={styles.attendeesImgBox}>
-              <div className={styles.attendeesLink}>
-                <div className={styles.attendeesImgRounded}></div>
-                <p className={styles.attendeesName}>Lucia</p>
+          {eventData ? (
+            <>
+              <header>Speaker</header>
+              <p className={styles.hostName}>Angelo Sarmiento</p>
+              <div className={styles.attendees}>
+                <div className={styles.attendeesTopBox}>
+                  <p className={styles.attendeesCount}>Attendees(31)</p>
+                  <p
+                    onClick={() => setAttendeesMenuIsOpen(true)}
+                    className={styles.attendeesSeeAll}
+                  >
+                    See all
+                  </p>
+                </div>
+                <div className={styles.attendeesImgBox}>
+                  <div className={styles.attendeesLink}>
+                    <div className={styles.attendeesImgRounded}></div>
+                    <p className={styles.attendeesName}>Lucia</p>
+                  </div>
+                  <div className={styles.attendeesLink}>
+                    <div className={styles.attendeesImgRounded}></div>
+                    <p className={styles.attendeesName}>Lucia</p>
+                  </div>
+                  <div className={styles.attendeesLink}>
+                    <div className={styles.attendeesImgRounded}></div>
+                    <p className={styles.attendeesName}>Lucia</p>
+                  </div>
+                  <div
+                    onClick={() => setAttendeesMenuIsOpen(true)}
+                    className={styles.attendeesLink}
+                  >
+                    <div className={styles.attendeesImgRounded}></div>
+                    <p className={styles.attendeesMoreBtn}>More</p>
+                  </div>
+                  <div className={styles.attendeesMoreBox}>
+                    <div className={styles.attendeesImgRounded}></div>
+                  </div>
+                </div>
               </div>
-              <div className={styles.attendeesLink}>
-                <div className={styles.attendeesImgRounded}></div>
-                <p className={styles.attendeesName}>Lucia</p>
+            </>
+          ) : (
+            <>
+              <header>Speaker</header>
+              <Skeleton variant='text' sx={{ width: '12em', fontSize: '1.3em' }} />
+              <div className={styles.attendees}>
+                <div className={styles.attendeesTopBox}>
+                  <Skeleton variant='text' sx={{ width: '10em' }} />
+                </div>
+                <div className={styles.attendeesImgBox}>
+                  <div className={styles.attendeesLink}>
+                    <Skeleton
+                      variant='rectangular'
+                      sx={{ borderRadius: '50%', width: '9em', height: '12em' }}
+                    />
+                    <Skeleton variant='text' sx={{ width: '5em', fontSize: '1.2em' }} />
+                  </div>
+                  <div className={styles.attendeesLink}>
+                    <Skeleton
+                      variant='rectangular'
+                      sx={{ borderRadius: '50%', width: '9em', height: '12em' }}
+                    />
+                    <Skeleton variant='text' sx={{ width: '5em', fontSize: '1.2em' }} />
+                  </div>
+                  <div className={styles.attendeesLink}>
+                    <Skeleton
+                      variant='rectangular'
+                      sx={{ borderRadius: '50%', width: '9em', height: '12em' }}
+                    />
+                    <Skeleton variant='text' sx={{ width: '5em', fontSize: '1.2em' }} />
+                  </div>
+                  <div className={styles.attendeesLink}>
+                    <Skeleton
+                      variant='rectangular'
+                      sx={{ borderRadius: '50%', width: '9em', height: '12em' }}
+                    />
+                    <Skeleton variant='text' sx={{ width: '5em', fontSize: '1.2em' }} />
+                  </div>
+                </div>
               </div>
-              <div className={styles.attendeesLink}>
-                <div className={styles.attendeesImgRounded}></div>
-                <p className={styles.attendeesName}>Lucia</p>
-              </div>
-              <div onClick={() => setAttendeesMenuIsOpen(true)} className={styles.attendeesLink}>
-                <div className={styles.attendeesImgRounded}></div>
-                <p className={styles.attendeesMoreBtn}>More</p>
-              </div>
-              <div className={styles.attendeesMoreBox}></div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </section>
     </main>
