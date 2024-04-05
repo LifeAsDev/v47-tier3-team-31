5dfed0058b9352177f602c889a1711f0d4742a96
LifeAsDev <angelocraft551@gmail.com>
2024-04-05 17:37:07 -0400

    minor css fix header mobile menu

--- travel-event-host-app/src/components/header/Header.tsx
+++ travel-event-host-app/src/components/header/Header.tsx
@@ -166,8 +166,8 @@ export default function Header() {
         <svg
           onClick={() => setnavMenuIsOpen(false)}
           className={`${styles.closeIcon} ${styles.mobile}`}
-          width='2em'
-          height='2em'
+          width='1.75em'
+          height='1.75em'
           viewBox='0 0 24 24'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
