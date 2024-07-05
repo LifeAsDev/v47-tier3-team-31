d4f2ba3c46e0ea067cedd2ba3b00fc3598ac7bed
LifeAsDev <angelosarmiento5d@gmail.com>
2024-07-05 16:10:11 -0400

    footer minor and header minor fix

--- travel-event-host-app/src/components/footer/Footer.tsx
+++ travel-event-host-app/src/components/footer/Footer.tsx
@@ -1,12 +1,11 @@
-import styles from './footer.module.css'
-import Link from 'next/link'
+import styles from './footer.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
-    return(
-        <div className={styles.footer}>
-            <Link href="/about">About us</Link>
-            <a href="https://chingu.io/">Chingu</a>
-            <Link href="/team">Team</Link>
-        </div>
-    )
+  return (
+    <div className={styles.footer}>
+      <Link href='/about'>About us</Link>
+      <a href='https://chingu.io/'>Chingu</a>
+    </div>
+  );
 }
